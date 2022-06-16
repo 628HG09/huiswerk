3076f65f94477d983b3721e486996f1aff46dd58
SRutges <104904365+628HG09@users.noreply.github.com>
2022-06-16 17:05:01 +0200

    def. version huiswerk

--- loops.js
+++ loops.js
@@ -6,7 +6,10 @@
 // Hoera!
 // ==========================================
 
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera! ")
 
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -18,7 +21,12 @@
 // klaar!
  // ==========================================
 
-
+for (let i = 0; i < 5; i++) {
+    if (i<4){
+        console.log("loop");}
+    else {
+            console.log("Klaar!")}
+}
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -32,7 +40,12 @@
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
-
+for (let i = 0; i < 15; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " is even")}
+    else {
+        console.log(i + " is oneven")}
+}
 
 
 // ==========================================
@@ -50,4 +63,13 @@
 // >> 8
 // >> 9
 // ==========================================
+for (let i = 0; i < 10; i++) {
+    if (i<3) { console.log(i)}
+    else if (i < 6) {
+        console.log(">" + i)
+    }
+    else {
+        console.log(">>" + i)
+    }
 
+}
